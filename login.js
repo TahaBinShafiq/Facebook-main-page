ef57d0fcb141e471e14d30333bdf43fe05bd467f
Taha Bin Shafiq <muhammadtahaaa7@gmail.com>
2025-08-06 21:49:34 +0500

    edit password input

--- login.js
+++ login.js
@@ -1,3 +1,32 @@
+let passInput = document.getElementById("loginPassword");
+let passEye = document.getElementById("eye-img");
+let NewPasswordEyeImg = document.getElementById("new-eye-img")
+let NewPasswordInp = document.getElementById("NewPasswordInput")
+
+function passwordTypeChnge() {
+    if (passInput && passEye) {
+        if (passInput.type === "password") {
+            passInput.type = "text";
+            passEye.src = "https://cdn-icons-png.flaticon.com/512/159/159604.png";
+        } else {
+            passInput.type = "password";
+            passEye.src = "https://cdn-icons-png.flaticon.com/512/709/709612.png";
+        }
+    }
+
+    if (NewPasswordInp && NewPasswordEyeImg) {
+        if (NewPasswordInp.type === "password") {
+            NewPasswordInp.type = "text";
+            NewPasswordEyeImg.src = "https://cdn-icons-png.flaticon.com/512/159/159604.png";
+        } else {
+            NewPasswordInp.type = "password";
+            NewPasswordEyeImg.src = "https://cdn-icons-png.flaticon.com/512/709/709612.png";
+        }
+    }
+};
+
+
+
 class Person {
     fullName
     email
@@ -6,8 +35,8 @@ class Person {
     friends
     constructor(fullName, email, password, id) {
         this.fullName = fullName,
-        this.email = email,
-        this.password = password
+            this.email = email,
+            this.password = password
         this.id = id
         this.friends = []
     }
@@ -25,13 +54,12 @@ function resgisterUser(event) {
     }
     let fullName = document.getElementById("fullName");
     let email = document.getElementById("email");
-    let password = document.getElementById("password");
     let para = document.getElementById("para")
 
     let usersFormStorage = JSON.parse(localStorage.getItem("users")) || []
 
     let savedUser = usersFormStorage.find((element) => element.email === email.value)
-    
+
     if (savedUser?.email) {
         para.style.display = "block"
         para.innerHTML = "This user is already registered"
@@ -40,14 +68,14 @@ function resgisterUser(event) {
         password.value = "";
     } else {
         let newId = usersFormStorage.length + 1;
-        let newUser = new Person(fullName.value, email.value, password.value, newId);
+        let newUser = new Person(fullName.value, email.value, NewPasswordInp.value, newId);
         usersFormStorage.push(newUser);
         console.log(usersFormStorage);
         localStorage.setItem("users", JSON.stringify(usersFormStorage))
         para.style.display = "none"
         fullName.value = ""
         email.value = ""
-        password.value = ""
+        NewPasswordEyeImg.value = ""
         Swal.fire({
             title: "Your account has been registered.",
             icon: "success",
@@ -67,18 +95,18 @@ function resgisterUser(event) {
 function loginUser(event) {
     event.preventDefault();
     let email = document.getElementById("login-email");
-    let password = document.getElementById("login-password");
+    
 
     let usersFormStorage = JSON.parse(localStorage.getItem("users")) || []
 
     let savedUser = usersFormStorage.find((element) => element.email === email.value)
 
-    if (savedUser?.email === email.value && savedUser?.password === password.value) {
+    if (savedUser?.email === email.value && savedUser?.password === passInput.value) {
         if (!Array.isArray(savedUser.friends)) {
             savedUser.friends = [];
         }
         email.value = ""
-        password.value = ""
+        passInput.value = ""
         localStorage.setItem("loginUser", JSON.stringify(savedUser));
         window.location.assign("MainPage/index.html")
     } else {
